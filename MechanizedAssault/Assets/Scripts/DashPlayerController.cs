dbf89146acc5a82ce7785a7e0b2849d6f710c969
John_Miele <john_miele@bloomfield.edu>
2017-03-17 22:43:30 -0400

    Update to change committed status

---
+++ MechanizedAssault/Assets/Scripts/DashPlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DashPlayerController : NetworkBehaviour {
+	//private GameObject LightFrame;
+	public Rigidbody LRB;
+	[SyncVar]
+	public float speed = 30.0f;
+
+
+	void Start () {
+		//LightFrame = GameObject.FindGameObjectWithTag ("LightFrame");
+	}
+
+	void Update () {
+		Debug.Log ("Void Update is running");
+		if (isLocalPlayer && hasAuthority) {
+			Debug.Log ("Just found local player && Authority");
+			if (Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")) {
+				Debug.Log ("Bout to call input");
+				GetInput ();
+				Debug.Log ("Just called input");
+			}
+		}
+	}
+
+	void GetInput(){
+		Debug.Log ("GetInput was just called");
+		float moveX = Input.GetAxis ("Horizontal") * speed + Time.deltaTime;
+		float moveZ = Input.GetAxis ("Vertical") * speed + Time.deltaTime;
+
+		if (isServer) {
+			Debug.Log ("Calling RPC");
+			RpcMove (moveX, moveZ);
+		} else {
+			Debug.Log ("Calling Command");
+			CmdMove (moveX, moveZ);
+		}
+			
+	}
+
+
+	[ClientRpc]
+	void RpcMove(float movex, float movez){
+		Debug.Log("Starting RPC movement");
+
+		Vector3 movement = new Vector3 (movex, 0.0f, movez);
+		LRB.velocity = movement * speed;
+	}
+
+	[Command]
+	void CmdMove(float movex, float movez){
+		Debug.Log ("Starting CMD movement");
+		RpcMove (movex, movez);
+		//Vector3 movement = new Vector3 (movex, 0.0f, movez);
+		//LRB.velocity = movement * speed;
+	}
+}
---
+++ MechanizedAssault/Assets/Scripts/DashPlayerControllerSP.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashPlayerControllerSP : MonoBehaviour {
+
+	public int Gspeed = 27;
+	public int Jspeed = 34;
+	public Rigidbody LRB;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		float moveX = Input.GetAxis ("Horizontal") * Gspeed * Time.deltaTime;
+		float moveZ = Input.GetAxis ("Vertical") * Gspeed * Time.deltaTime;
+		float moveY = Input.GetAxis ("Jump") * Jspeed * Time.deltaTime;
+
+		if (Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")) {
+			Debug.Log ("Bout to call input");
+			Move(moveX, moveZ);
+			Debug.Log ("Just called input");
+		}
+
+		if (Input.GetButton ("Jump")){
+			Fly(moveY);
+		}
+	}
+
+	void Move(float movex, float movez){
+		Vector3 movement = new Vector3 (movex, 0.0f, movez);
+		LRB.velocity = movement * Gspeed;
+	}
+
+	void Fly (float movey){
+		Vector3 movement = new Vector3 (0.0f, movey, 0.0f);
+		LRB.velocity = movement * Jspeed;
+	}
+}
