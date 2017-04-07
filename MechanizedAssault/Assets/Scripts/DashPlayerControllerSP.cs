eeda2179d828104d15a78fdffc0ba9036115ac4a
John_Miele <john_miele@bloomfield.edu>
2017-04-07 16:15:19 -0400

    update at start of April, movement is janky but works, some models are into the game

--- MechanizedAssault/Assets/Scripts/DashPlayerControllerSP.cs
+++ MechanizedAssault/Assets/Scripts/DashPlayerControllerSP.cs
@@ -7,12 +7,16 @@ public class DashPlayerControllerSP : MonoBehaviour {
 	public int Gspeed = 27;
 	public int Jspeed = 34;
 	public Rigidbody LRB;
+	public bool OnGround=true;
 
 	void Start () {
 		
 	}
 
 	void Update () {
+
+		OnGroundCheck ();
+
 		float moveX = Input.GetAxis ("Horizontal") * Gspeed * Time.deltaTime;
 		float moveZ = Input.GetAxis ("Vertical") * Gspeed * Time.deltaTime;
 		float moveY = Input.GetAxis ("Jump") * Jspeed * Time.deltaTime;
@@ -26,6 +30,10 @@ public class DashPlayerControllerSP : MonoBehaviour {
 		if (Input.GetButton ("Jump")){
 			Fly(moveY);
 		}
+		OnGroundCheck ();
+		if (OnGround == false) {
+			Debug.Log ("You are now off of the ground");
+		}
 	}
 
 	void Move(float movex, float movez){
@@ -36,5 +44,18 @@ public class DashPlayerControllerSP : MonoBehaviour {
 	void Fly (float movey){
 		Vector3 movement = new Vector3 (0.0f, movey, 0.0f);
 		LRB.velocity = movement * Jspeed;
+	}
+
+	void OnGroundCheck(){
+
+	}
+
+	void OnTriggerStay (Collider other){
+
+		if (other.tag == "Terrain") {
+			OnGround = true;
+		} else {
+			OnGround = false;
+		}
 	}
 }
